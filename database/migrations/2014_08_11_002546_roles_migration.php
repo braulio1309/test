f61ac5f1e9ba943fb5c2b8f8e93167c843d1b106
braulio1309 <brauliozapatad@gmail.com>
2021-08-19 09:29:18 -0400

    Modificaciones

--- database/migrations/2014_08_11_002546_roles_migration.php
+++ database/migrations/2014_08_11_002546_roles_migration.php
@@ -14,7 +14,7 @@ class RolesMigration extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('nombre', 50);
             $table->timestamps();
         });
