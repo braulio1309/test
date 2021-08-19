345d7fdb7c058c7508c5bef9cacc5d0d15627c6c
braulio1309 <brauliozapatad@gmail.com>
2021-08-19 10:34:48 -0400

    Seeders y validaciones

--- database/migrations/2021_08_18_193523_categorias_productos_migration.php
+++ database/migrations/2021_08_18_193523_categorias_productos_migration.php
@@ -14,12 +14,15 @@ class CategoriasProductosMigration extends Migration
     public function up()
     {
         Schema::create('categorias_productos', function (Blueprint $table) {
-            $table->integer('producto_id')->unsigned()->primary();
-            $table->integer('categoria_id')->unsigned()->index();
+            $table->id();
+            $table->integer('producto_id')->unsigned();
+            $table->integer('categoria_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
 
             $table->foreign('producto_id')->references('id')->on('productos');
             $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
